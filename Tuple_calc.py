703d4296ca5a2707a3c2187bc2ce70b9a32c4f6c
emilwiebach <emil.wiebach@gmail.com>
2026-05-09 10:13:43 +0200

    Update Tuple_calc.py

--- Tuple_calc.py
+++ Tuple_calc.py
@@ -398,8 +398,8 @@ for x in idemp:
     x[0] = x[0].replace("J", "J_{(")
     x[0] = x[0]+")}"
 
-with open("njm Latex.txt", "w") as f:
-    f.write("\\("+idemp[0][0]+"="+tupsumtolatex(idemp[0][1])+"\\)\\newline")
-with open("njm Latex.txt", "a") as f:
+with open("njm.txt", "w") as f:
+    f.write("\\("+idemp[0][0]+"="+tupsumtolatex(idemp[0][1])+"\\)\\newline" \n)
+with open("njm.txt", "a") as f:
     for x in idemp[1:len(idemp)]:
-        f.write("\\(" + x[0] + "=" + tupsumtolatex(x[1])+"\\)\\newline")
+        f.write("\\(" + x[0] + "=" + tupsumtolatex(x[1])+"\\)\\newline" \n)
